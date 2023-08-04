8509fe905192665cd21631b6d8ce76a6f97f9549
mrcbq <marcobaquero@gmail.com>
2023-08-04 12:06:18 -0500

    Add fizzbuzz function and pass the tests
    Co-authored-by: lincoln1883 <lincolngibson7@gmail.com>

--- solver.rb
+++ solver.rb
@@ -22,4 +22,12 @@ class Solver
   def reverse(str)
     str.reverse
   end
+
+  def fizzbuzz(num)
+    return 'fizzbuzz' if num % 15 == 0
+    return 'fizz' if num % 3 == 0
+    return 'buzz' if num % 5 == 0
+
+    num.to_s
+  end
 end
