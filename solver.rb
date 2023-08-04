5bc3319972df03eeae9589775ef92ae606b936f2
mrcbq <marcobaquero@gmail.com>
2023-08-04 12:07:52 -0500

    Fix the linters errors
    Co-authored-by: lincoln1883 <lincolngibson7@gmail.com>

--- solver.rb
+++ solver.rb
@@ -1,32 +1,27 @@
 require 'pry'
 
 class Solver
+  def factorial(num)
+    arr = (1..num)
+    sum = 1
+    return 'Number should not be negative.' if num.negative?
 
-	def factorial(n)
-		arr = (1..n)
-		sum = 1
-		if n.negative?
-			return "Number should not be negative."
-		end
+    return 1 if [0, 1].include?(num)
 
-		if (n == 0 || n == 1) 
-			return 1 			
-		else  
-			arr.each do |ele|
-				sum *= ele
-			end
-			return sum
-		end
-	end
+    arr.each do |ele|
+      sum *= ele
+    end
+    sum
+  end
 
   def reverse(str)
     str.reverse
   end
 
   def fizzbuzz(num)
-    return 'fizzbuzz' if num % 15 == 0
-    return 'fizz' if num % 3 == 0
-    return 'buzz' if num % 5 == 0
+    return 'fizzbuzz' if (num % 15).zero?
+    return 'fizz' if (num % 3).zero?
+    return 'buzz' if (num % 5).zero?
 
     num.to_s
   end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Solver do
 
     it 'returns the factorial number for positive integers' do
       expect(solver.factorial(5)).to eq(120)
-      expect(solver.factorial(9)).to eq(362880)
+      expect(solver.factorial(9)).to eq(362_880)
     end
 
     it 'Throw an ArgumentError for negative numbers' do
@@ -26,27 +26,27 @@ RSpec.describe Solver do
     end
   end
 
-	describe '#fizz-buzz' do
-		it 'return fizz for multiples of 3' do
-		expect(solver.fizzbuzz(3)).to eq('fizz')
-		expect(solver.fizzbuzz(6)).to eq('fizz')
-		end
+  describe '#fizz-buzz' do
+    it 'return fizz for multiples of 3' do
+      expect(solver.fizzbuzz(3)).to eq('fizz')
+      expect(solver.fizzbuzz(6)).to eq('fizz')
+    end
 
-		it 'returns buzz for multiples of 5' do
-		expect(solver.fizzbuzz(5)).to eq('buzz')
-		expect(solver.fizzbuzz(10)).to eq('buzz')
-		end
+    it 'returns buzz for multiples of 5' do
+      expect(solver.fizzbuzz(5)).to eq('buzz')
+      expect(solver.fizzbuzz(10)).to eq('buzz')
+    end
 
-		it 'returns fizzbuzz for mulitiples of 3 and 5' do
-		expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
-		expect(solver.fizzbuzz(30)).to eq('fizzbuzz')
-		expect(solver.fizzbuzz(45)).to eq('fizzbuzz')
-		end
+    it 'returns fizzbuzz for mulitiples of 3 and 5' do
+      expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+      expect(solver.fizzbuzz(30)).to eq('fizzbuzz')
+      expect(solver.fizzbuzz(45)).to eq('fizzbuzz')
+    end
 
-		it 'returns number as a string if not a multiple of 3 or 5' do
-			expect(solver.fizzbuzz(1)).to eq('1')
-			expect(solver.fizzbuzz(2)).to eq('2')
-			expect(solver.fizzbuzz(4)).to eq('4')
-		end
-	end
+    it 'returns number as a string if not a multiple of 3 or 5' do
+      expect(solver.fizzbuzz(1)).to eq('1')
+      expect(solver.fizzbuzz(2)).to eq('2')
+      expect(solver.fizzbuzz(4)).to eq('4')
+    end
+  end
 end
