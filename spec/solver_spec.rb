9d6d2f9300818f83bb6d59b6765cffe6d1fb1136
mrcbq <marcobaquero@gmail.com>
2023-08-04 10:15:39 -0500

    Add factorial tests
    Co-authored-by: lincoln1883 <lincolngibson7@gmail.com>

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -4,8 +4,17 @@ RSpec.describe Spec::Solver do
   let(:solver) { Solver.new }
 
   describe '#factorial' do
-    it 'rwturns 1 for factorial of 0' do
-      expect(solver.factorial).to eq(1)
+    it 'returns 1 for factorial of 0' do
+      expect(solver.factorial(1)).to eq(1)
+    end
+
+    it 'returns the factorial number for positive integers' do
+      expect(solver.factorial(5)).to eq(120)
+      expect(solver.factorial(9)).to eq(362880)
+    end
+
+    it 'Throw an ArgumentError for negative numbers' do
+      expect(solver.factorial(-5)).to raise_error(ArgumentError)
     end
   end
 end
